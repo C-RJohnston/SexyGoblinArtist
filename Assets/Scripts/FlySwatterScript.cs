0f2f0b83a7f8a5f20dfa9ae3896d277388201c27
Cameron Johnston <cameron.robin.johnston@gmail.com>
2023-10-25 16:30:57 +0100

    start fly swatter

---
+++ Assets/Scripts/FlySwatterScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlySwatterScript : MonoBehaviour
+{
+
+    [SerializeField] private Texture2D cursorTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
