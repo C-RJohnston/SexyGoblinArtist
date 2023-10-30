946c3ba08c01046e6051203f056afecc0678ad90
Doruk Balci <c.d.balci@gmail.com>
2023-10-30 11:18:05 +0000

    Button Code Refactor

--- Assets/Scripts/ActivateTool.cs
+++ Assets/Scripts/ActivateTool.cs
@@ -5,15 +5,34 @@ using UnityEngine.UI;
 
 public class ActivateTool : MonoBehaviour
 {
-    public GameObject toActivate;
+    public GameObject drawTool;
+    public GameObject eraseTool;
+    public GameObject flySwatter;
+
     public GameObject[] tools;
 
-    public void OnButtonClick()
+    public void actDraw()
     {
-        foreach(GameObject tool in tools)
+        foreach (GameObject tool in tools)
         {
             tool.SetActive(false);
         }
-        toActivate.SetActive(true);
+        drawTool.SetActive(true);
+    }
+    public void actErase()
+    {
+        foreach (GameObject tool in tools)
+        {
+            tool.SetActive(false);
+        }
+        eraseTool.SetActive(true);
+    }
+    public void actFly()
+    {
+        foreach (GameObject tool in tools)
+        {
+            tool.SetActive(false);
+        }
+        flySwatter.SetActive(true);
     }
 }
